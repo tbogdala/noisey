b3c80ab1cbd83cf86e333b49d1e0e231336a549c
Timothy Bogdala <tdb@animal-machine.com>
2016-03-23 22:05:14 -0500

    added 3d fBm and select modules

--- fbm.go
+++ fbm.go
@@ -53,3 +53,45 @@ func (fbm *FBMGenerator2D) Get2D(x float64, y float64) (v float64) {
 
 	return
 }
+
+// FBMGenerator3D takes noise and makes fractal Brownian motion values.
+type FBMGenerator3D struct {
+	NoiseMaker  NoiseyGet3D // the interface FBMGenerator3D uses gets noise values
+	Octaves     int         // the number of octaves to calculate on each Get()
+	Persistence float64     // a multiplier that determines how quickly the amplitudes diminish for each successive octave
+	Lacunarity  float64     // a multiplier that determines how quickly the frequency increases for each successive octave
+	Frequency   float64     // the number of cycles per unit length
+}
+
+// NewFBMGenerator3D creates a new fractal Brownian motion generator state. A 'default' fBm
+// would have 1 octave, 0.5 persistence, 2.0 lacunarity and 1.0 frequency.
+func NewFBMGenerator3D(noise NoiseyGet3D, octaves int, persistence float64, lacunarity float64, frequency float64) (fbm FBMGenerator3D) {
+	fbm.NoiseMaker = noise
+	fbm.Octaves = octaves
+	fbm.Persistence = persistence
+	fbm.Lacunarity = lacunarity
+	fbm.Frequency = frequency
+	return
+}
+
+// Get3D calculates the noise value over the number of Octaves and other parameters
+// that scale the coordinates over each octave.
+func (fbm *FBMGenerator3D) Get3D(x float64, y float64, z float64) (v float64) {
+	curPersistence := 1.0
+
+	x *= fbm.Frequency
+	y *= fbm.Frequency
+	z *= fbm.Frequency
+
+	for o := 0; o < fbm.Octaves; o++ {
+		signal := fbm.NoiseMaker.Get3D(x, y, z)
+		v += signal * curPersistence
+
+		x *= fbm.Lacunarity
+		y *= fbm.Lacunarity
+		z *= fbm.Lacunarity
+		curPersistence *= fbm.Persistence
+	}
+
+	return v
+}
--- select.go
+++ select.go
@@ -83,3 +83,84 @@ func (selector *Select2D) Get2D(x float64, y float64) (v float64) {
 	}
 	return selector.SourceA.Get2D(x, y)
 }
+
+// Select3D is a module that uses SourcesA or SourceB depending
+// on the value coming from Control. If the value from control is between
+// LowerBound and UpperBound then it uses SourceB, but otherwise it will
+// use SourceA.
+type Select3D struct {
+	// the first channel of noise that the select module uses
+	SourceA NoiseyGet3D
+
+	// the second channel of noise that the select module uses
+	SourceB NoiseyGet3D
+
+	// a channel of noise that determines whether SourceA or SourceB gets
+	// used as an output for a given coordinate
+	Control NoiseyGet3D
+
+	// if the value of Control is above LowerBound but below Upper Bound
+	// then SourceB is output, otherwise SourceA is output
+	LowerBound float64
+
+	// if the value of Control is above LowerBound but below Upper Bound
+	// then SourceB is output, otherwise SourceA is output
+	UpperBound float64
+
+	// the folloff value at the edge of a transition -- if <=0.0 there is an
+	// abrupt transition from SourceA to SourceB, otherwise it specifies
+	// the width of the transition range where values are blended between
+	// the two sources
+	EdgeFalloff float64
+}
+
+// NewSelect3D creates a new selector 3d module.
+func NewSelect3D(a, b, c NoiseyGet3D, lower float64, upper float64, edge float64) (selector Select3D) {
+	selector.SourceA = a
+	selector.SourceB = b
+	selector.Control = c
+	selector.LowerBound = lower
+	selector.UpperBound = upper
+	selector.EdgeFalloff = edge
+	return
+}
+
+// Get3D calculates the noise value using SourceA or SourceB depending on Control.
+func (selector *Select3D) Get3D(x, y, z float64) (v float64) {
+	control := selector.Control.Get3D(x, y, z)
+
+	// process the lack of edge falloff first
+	if selector.EdgeFalloff <= 0.0 {
+		if selector.LowerBound < control && control < selector.UpperBound {
+			return selector.SourceB.Get3D(x, y, z)
+		}
+		return selector.SourceA.Get3D(x, y, z)
+	}
+
+	// if we got here, then edge falloff is positive
+	if control < selector.LowerBound-selector.EdgeFalloff {
+		return selector.SourceA.Get3D(x, y, z)
+	}
+	if control < selector.LowerBound+selector.EdgeFalloff {
+		lower := selector.LowerBound - selector.EdgeFalloff
+		upper := selector.LowerBound + selector.EdgeFalloff
+		v := (control - lower) / (upper - lower)
+		lerpControl := calcCubicSCurve(v)
+		a := selector.SourceA.Get3D(x, y, z)
+		b := selector.SourceB.Get3D(x, y, z)
+		return lerp(a, b, lerpControl)
+	}
+	if control < selector.UpperBound-selector.EdgeFalloff {
+		return selector.SourceB.Get3D(x, y, z)
+	}
+	if control < selector.UpperBound+selector.EdgeFalloff {
+		lower := selector.UpperBound - selector.EdgeFalloff
+		upper := selector.UpperBound + selector.EdgeFalloff
+		v := (control - lower) / (upper - lower)
+		lerpControl := calcCubicSCurve(v)
+		a := selector.SourceA.Get3D(x, y, z)
+		b := selector.SourceB.Get3D(x, y, z)
+		return lerp(b, a, lerpControl)
+	}
+	return selector.SourceA.Get3D(x, y, z)
+}
